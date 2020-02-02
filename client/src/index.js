8fd26043396f8561d6c4685f7595319994c52d1c
David S <david@vendrix.co>
2020-02-02 17:04:44 -0600

    Got the nave bar working

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import manageBlog from './reducers/manageBlog'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(manageBlog,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
+ReactDOM.render(
+<Provider store={store}> 
+    <App />
+</Provider>
+
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ client/src/reducers/manageBlog.js
@@ -0,0 +1,11 @@
+import cuid from 'cuid';
+export const cuidFn = cuid; 
+
+export default function manageBlog(state = {
+
+}, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
