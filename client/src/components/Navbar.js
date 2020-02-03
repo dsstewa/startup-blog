a624c51f586060ae0b5680f4f04b6d7eee1c1da0
David S <david@vendrix.co>
2020-02-03 14:45:37 -0600

    working on getting sass setup

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Navbar extends Component {
-
-    render() {
-        
-        return (      
-          <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-              <div className="container">
-                 <a href="index.html" className="navbar-brand">Vendrix</a>
-                     <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                         <span className="navbar-toggler-icon"></span>
-                     </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-
-              <ul className="navbar-nav mr-auto">
-                  <li className="nav-item">
-                  <a href="#home" className="nav-link">Home</a>
-                  </li>
-                    <li className="nav-item">
-                      <a href="#explore-head-section" className="nav-link">Blog</a>
-                    </li>
-                </ul>
-
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item">
-                      <a href="#create-head-section" className="nav-link">Signin</a>
-                    </li>
-                 </ul>
-            </div>
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+        <div className="container">
+          <a href="index.html" className="navbar-brand">
+            Vendrix
+          </a>
+          <button
+            className="navbar-toggler"
+            data-toggle="collapse"
+            data-target="#navbarCollapse"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+            <ul className="navbar-nav mr-auto">
+              <li className="nav-item">
+                <a href="#home" className="nav-link">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="#explore-head-section" className="nav-link">
+                  Blog
+                </a>
+              </li>
+            </ul>
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a href="#create-head-section" className="nav-link">
+                  Signin
+                </a>
+              </li>
+            </ul>
           </div>
-        </nav>
-       
-            
-      )}
+        </div>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
