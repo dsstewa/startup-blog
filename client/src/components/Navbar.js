37405a3d7b60dc5a7fcf394b4359cacb9a4440c6
David S <david@vendrix.co>
2020-02-03 14:08:50 -0600

    added the sass package

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,33 +4,33 @@ class Navbar extends Component {
 
     render() {
         
-        return (
-       <div>
+        return (      
           <nav className="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-          <div className="container">
-            <a href="index.html" className="navbar-brand">LooLap</a>
-            <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+              <div className="container">
+                 <a href="index.html" className="navbar-brand">Vendrix</a>
+                     <button className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                          <span className="navbar-toggler-icon"></span>
                      </button>
             <div className="collapse navbar-collapse" id="navbarCollapse">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
+
+              <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
                   <a href="#home" className="nav-link">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#explore-head-section" className="nav-link">Explore</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#create-head-section" className="nav-link">Create</a>
-                </li>
-                <li className="nav-item">
-                  <a href="#share-head-section" className="nav-link">Share</a>
-                </li>
-              </ul>
+                  </li>
+                    <li className="nav-item">
+                      <a href="#explore-head-section" className="nav-link">Blog</a>
+                    </li>
+                </ul>
+
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                      <a href="#create-head-section" className="nav-link">Signin</a>
+                    </li>
+                 </ul>
             </div>
           </div>
         </nav>
-        </div>
+       
             
       )}
 }
