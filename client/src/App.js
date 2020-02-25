3ccede020ac0bada849f01bcce5bfb11691c985d
dstewart <davidstewart@gmail.com>
2020-02-24 17:23:10 -0700

    Fixed bug with navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Homecontainer from "./containers/Homecontainer";
 import Blogcontainer from "./containers/Blogcontainer";
 import Newblogpost from "./containers/Newblogpost";
@@ -10,9 +10,9 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <Route exact path="/" component={Homecontainer} />
-      <Route exact path="/blog" component={Blogcontainer} />
-      <Route exact path="/blog/new" component={Newblogpost} />
+      <Route exact path='/' component={Homecontainer} />
+      <Route exact path='/blog' component={Blogcontainer} />
+      <Route exact path='/blog/new' component={Newblogpost} />
     </Router>
   );
 }
